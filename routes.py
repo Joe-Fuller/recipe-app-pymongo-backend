1d8dc27e4c240a0e694af2ab21f1419e624be654
Joe <joefuller042@gmail.com>
2023-07-01 15:36:34 +0100

    add POST /recipe

--- routes.py
+++ routes.py
@@ -5,3 +5,19 @@ from typing import List, Dict
 from models import Recipe, RecipeUpdate
 
 router = APIRouter()
+
+
+@router.post(
+    "/",
+    response_description="Create a new recipe",
+    status_code=status.HTTP_201_CREATED,
+    response_model=Recipe,
+)
+def create_recipe(request: Request, recipe: Recipe = Body(...)):
+    recipe = jsonable_encoder(recipe)
+    new_recipe = request.app.database["recipes"].insert_one(recipe)
+    created_recipe = request.app.database["recipes"].find_one(
+        {"_id", new_recipe.inserted_id}
+    )
+
+    return created_recipe
