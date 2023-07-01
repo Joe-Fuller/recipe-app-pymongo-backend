debb50345273316fc06aff5ebd85abe04b0887cc
Joe <joefuller042@gmail.com>
2023-07-01 15:32:41 +0100

    add RecipeUpdate schema and add examples to each

--- models.py
+++ models.py
@@ -16,3 +16,34 @@ class Recipe(BaseModel):
     image: str = Field(...)
     ingredients = List[Dict[str, str]] = Field(...)
     instructions = List[str] = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "_id": "1",
+                "name": "Trifle",
+                "timeToCook": "1 Hour",
+                "image": "http:image.com/recipeimage",
+                "ingredients": [{"name": "milk", "amount": "2", "units": "cups"}],
+                "instructions": ["Make trifle", "Serve trifle"],
+            }
+        }
+
+
+class RecipeUpdate(BaseModel):
+    name: Optional[str]
+    timeToCook: Optional[str]
+    image: Optional[str]
+    ingredients: Optional[List[Dict[str, str]]]
+    instructions: Optional[List[str]]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Trifle",
+                "timeToCook": "1 Hour",
+                "image": "http:image.com/recipeimage",
+                "ingredients": [{"name": "milk", "amount": "2", "units": "cups"}],
+                "instructions": ["Make trifle", "Serve trifle"],
+            }
+        }
