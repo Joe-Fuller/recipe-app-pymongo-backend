04769ab125c5b212bbd3662bc950bb3258aebfe2
Joe <joefuller042@gmail.com>
2023-07-01 16:00:28 +0100

    create GET /recipe/{id}

--- routes.py
+++ routes.py
@@ -27,3 +27,14 @@ def create_recipe(request: Request, recipe: Recipe = Body(...)):
 def list_recipes(request: Request):
     recipes = list(request.app.database["recipes"]).find(limit=100)
     return recipes
+
+
+@router.get(
+    "/{id}", response_description="Get a single recipe by id", response_model=Recipe
+)
+def find_recipe(id: str, request: Request):
+    if (recipe := request.app.database["recipes"].find_one({"_id": id})) is not None:
+        return recipe
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Recipe with ID {id} not found"
+    )
