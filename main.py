98ac48d001fa1afae3bd4e5041372fc9a3cb0978
Joe <joefuller042@gmail.com>
2023-07-01 14:48:50 +0100

    update main.py with startup, shutdown, and credentials

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+config = dotenv_values(".env")
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "You've done the first thing"}
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
