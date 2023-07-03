a8f110d4ced975688bb204fa95af5a26992820e5
Joe <joefuller042@gmail.com>
2023-07-03 13:39:16 +0100

    successfully extract all data from bbcgoodfood

--- utils/scrapeRecipe.py
+++ utils/scrapeRecipe.py
@@ -1,10 +1,31 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import json
 
 html = urllib.request.urlopen("https://www.bbcgoodfood.com/recipes/vegan-sponge").read()
 
 soup = BeautifulSoup(html, "html.parser")
 
 # scripts = soup.find_all("scripts")
-scripts = soup.find_all(lambda tag: tag.name == "script" and "schema" in tag.text)
-print(len(scripts))
+scripts = soup.find_all(
+    lambda tag: tag.name == "script" and "recipeIngredient" in tag.text
+)
+
+schema = scripts[0]
+text = schema.get_text()
+data = json.loads(text)
+
+# instructions = data["recipeInstructions"]
+# print(instructions[0]["text"])
+
+
+recipeData = {
+    "name": data["name"],
+    "timeToCook": data["totalTime"],
+    "ingredients": data["recipeIngredient"],
+    "instructions": [x["text"] for x in data["recipeInstructions"]],
+    "imageUrl": data["image"]["url"],
+}
+
+
+print(recipeData)
