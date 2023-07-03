c15bafa3b6556f3efbab0eadbf6e4e4bb7d9f7ca
Joe <joefuller042@gmail.com>
2023-07-03 17:34:29 +0100

    successfully create database and insert item

--- utils/pymongo_test_insert.py
+++ utils/pymongo_test_insert.py
@@ -1,4 +1,15 @@
 from getDatabase import get_database
 
 dbname = get_database()
-collection_name = dbname["recipe1"]
+collection_name = dbname["recipes"]
+
+
+recipe_1 = {
+    "name": "Trifle",
+    "timeToCook": "1 Hour",
+    "image": "http:image.com/recipeimage",
+    "ingredients": [{"name": "milk", "amount": "2", "units": "cups"}],
+    "instructions": ["Make trifle", "Serve trifle"],
+}
+
+collection_name.insert_one(recipe_1)
