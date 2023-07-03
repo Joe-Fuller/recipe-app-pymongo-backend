9ea90bab5e985b5592120ce94957c1a93c6bfc7c
Joe <joefuller042@gmail.com>
2023-07-03 20:11:41 +0100

    make scrape_recipe a function

--- utils/scrapeRecipe.py
+++ utils/scrapeRecipe.py
@@ -3,26 +3,28 @@ import urllib.request
 import json
 from saveRecipe import save_recipe
 
-html = urllib.request.urlopen("https://www.bbcgoodfood.com/recipes/vegan-sponge").read()
 
-soup = BeautifulSoup(html, "html.parser")
+def scrape_recipe(url):
+    html = urllib.request.urlopen(url).read()
 
-scripts = soup.find_all(
-    lambda tag: tag.name == "script" and "recipeIngredient" in tag.text
-)
+    soup = BeautifulSoup(html, "html.parser")
 
-schema = scripts[0]
-text = schema.get_text()
-data = json.loads(text)
+    scripts = soup.find_all(
+        lambda tag: tag.name == "script" and "recipeIngredient" in tag.text
+    )
 
-recipeData = {
-    "name": data["name"],
-    "timeToCook": data["totalTime"],
-    "ingredients": data["recipeIngredient"],
-    "instructions": [x["text"] for x in data["recipeInstructions"]],
-    "imageUrl": data["image"]["url"],
-}
+    schema = scripts[0]
+    text = schema.get_text()
+    data = json.loads(text)
 
-print(recipeData)
+    recipeData = {
+        "name": data["name"],
+        "timeToCook": data["totalTime"],
+        "ingredients": data["recipeIngredient"],
+        "instructions": [x["text"] for x in data["recipeInstructions"]],
+        "imageUrl": data["image"]["url"],
+    }
 
-save_recipe(recipeData)
+    print(recipeData)
+
+    save_recipe(recipeData)
