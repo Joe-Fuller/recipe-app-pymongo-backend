5fe6dab44b74ff436e4eb6e9867ea46ec55f036b
Joe <joefuller042@gmail.com>
2023-07-04 16:28:21 +0100

    update_recipe working

--- database_functions.py
+++ database_functions.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from dotenv import dotenv_values
+from bson.objectid import ObjectId
 
 config = dotenv_values(".env")
 
@@ -27,6 +28,10 @@ def save_recipe(recipe):
 
 def delete_recipe(recipe_id):
     collection.delete_one({"_id": recipe_id})
+
+
+def update_recipe(recipe_id, updates):
+    collection.update_one({"_id": ObjectId(recipe_id)}, {"$set": updates}, upsert=True)
 
 
 if __name__ == "__main__":
--- testing.py
+++ testing.py
@@ -1,9 +1,11 @@
-from database_functions import get_recipes, delete_recipe
+from database_functions import get_recipes, delete_recipe, update_recipe
 
-recipes = get_recipes()
-print([recipe["_id"] for recipe in recipes])
+# recipes = get_recipes()
+# print([recipe["_id"] for recipe in recipes])
 
-delete_recipe(recipes[0]["_id"])
+# delete_recipe(recipes[0]["_id"])
 
-recipes = get_recipes()
-print([recipe["_id"] for recipe in recipes])
+# recipes = get_recipes()
+# print([recipe["_id"] for recipe in recipes])
+
+update_recipe("64a419a8b6762c9e029ade95", {"name": "test2"})
