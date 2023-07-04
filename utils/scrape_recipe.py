b72c1d1b6c6c2f8ede1c6ddaa3bae6f876bf2dcb
Joe <joefuller042@gmail.com>
2023-07-04 14:02:05 +0100

    split ingredient strings

--- utils/scrape_recipe.py
+++ utils/scrape_recipe.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 import json
 from utils.save_recipe import save_recipe
+import split_ingredient_string
 
 
 def scrape_recipe(url):
@@ -17,13 +18,15 @@ def scrape_recipe(url):
     text = schema.get_text()
     data = json.loads(text)
 
-    recipeData = {
-        "name": data["name"],
-        "timeToCook": data["totalTime"],
-        "ingredients": data["recipeIngredient"],
-        "instructions": [x["text"] for x in data["recipeInstructions"]],
-        "imageUrl": data["image"]["url"],
-    }
+    name = data["name"]
+    timeToCook = data["totalTime"]
+    ingredients = [
+        split_ingredient_string(ingredient) for ingredient in data["recipeIngredient"]
+    ]
+    instructions = [instruction["text"] for instruction in data["recipeInstructions"]]
+    imageUrl = data["image"]["url"]
+
+    recipeData = {name, timeToCook, ingredients, instructions, imageUrl}
 
     print(recipeData)
 
