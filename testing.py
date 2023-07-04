910cbadec36be13dc9555a656afd428c64f43e56
Joe <joefuller042@gmail.com>
2023-07-04 16:49:36 +0100

    get all recipe names from one page of bbcgoodfood

--- testing.py
+++ testing.py
@@ -1,4 +1,8 @@
 from database_functions import get_recipes, delete_recipe, update_recipe
+from bs4 import BeautifulSoup
+import urllib.request
+import json
+
 
 # recipes = get_recipes()
 # print([recipe["_id"] for recipe in recipes])
@@ -8,4 +12,18 @@ from database_functions import get_recipes, delete_recipe, update_recipe
 # recipes = get_recipes()
 # print([recipe["_id"] for recipe in recipes])
 
-update_recipe("64a419a8b6762c9e029ade95", {"name": "test2"})
+# update_recipe("64a419a8b6762c9e029ade95", {"name": "test2"})
+
+
+html = urllib.request.urlopen(
+    "https://www.bbcgoodfood.com/recipes/collection/vegan-recipes"
+).read()
+soup = BeautifulSoup(html, "html.parser")
+
+articles = soup.find_all("article")
+
+recipes = [article for article in articles if article.get("data-item-type") == "recipe"]
+
+recipe_names = [recipe["data-item-name"] for recipe in recipes]
+
+print(recipe_names)
